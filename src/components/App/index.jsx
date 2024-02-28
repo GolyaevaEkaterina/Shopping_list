70e564835b438461552986aab385f8fda360a95f
GolyaevaEkaterina <katrin.alien1@gmail.com>
2024-02-28 17:20:34 +0300

    Кнопки Добавить/Добавлено

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -8,27 +8,33 @@ function App() {
   const [elements, setElement] = useState([
     {
       id: 1,
-      name: "Апельсины"
+      name: "Апельсины",
+      added: false
     },
     {
       id: 2,
-      name: "Сыр"
+      name: "Сыр",
+      added: false
     },
     {
       id: 3,
-      name: "Вино"
+      name: "Вино",
+      added: false
     },
     {
       id: 4,
-      name: "Порошок"
+      name: "Порошок",
+      added: false
     },
     {
       id: 5,
-      name: "Минералка"
+      name: "Минералка",
+      added: false
     },
     {
       id: 6,
-      name: "Зубная щетка"
+      name: "Зубная щетка",
+      added: true
     }
   ])
 
@@ -36,15 +42,41 @@ function App() {
      const filteredElements = elements.filter(element => element.id !== id)
      setElement(filteredElements)
   }
+
+  
+
+
+
+  // function add(element){
+  //    if(element.added === false){
+  //      element.added = true
+  //      setElement(elements)
+  //    } 
+  //     element.added = false
+  //      setElement(elements)
+     
+  //    console.log(elements)
+  // }
+
  
   return (
     <div className="app">
       <Header />
       <div className="text-center max-w-screen-md mt-16 mx-auto px-8 py-8 bg-white rounded-md shadow-xl border-fuchsia-600 border-solid border-2 flex flex-col gap-y-10">
         {elements.map((element) => {
-          return(            
-            <Element key={element.id} element={element} onClick={() => deleteElement(element.id)}/>  
-          )
+          if(element.added === false){
+            return(            
+              <Element key={element.id} element={element} onClick={() => deleteElement(element.id)} number={elements.indexOf(element) + 1} add={"Добавлено"} />  
+            )
+          }
+          if(element.added === true){
+            return(            
+              <Element key={element.id} element={element} onClick={() => deleteElement(element.id)} number={elements.indexOf(element) + 1} add={"Добавить"} />  
+            )
+          }
+          // return(            
+          //   <Element key={element.id} element={element} onClick={() => deleteElement(element.id)} number={elements.indexOf(element) + 1} add={title}/>  
+          // )
         })}
       </div>
     </div>
--- src/components/Element/index.jsx
+++ src/components/Element/index.jsx
@@ -1,14 +1,15 @@
-import Button from "components/Button";
+
 
 function Element(props){
-    const { element, onClick } = props
+    const { element, onClick, number, add, onClickAdd} = props
     return(
         <div className="flex justify-between border-b-fuchsia-200 border-b-solid border-b-2">
             <div className="flex gap-4 text-2xl text-indigo-700 ">
-               <p>{element.id}.</p>
+               <p>{number}.</p>
                <p>{element.name}</p>
             </div>
-            <Button onClick={onClick(element.id)}/>
+            <button className="bg-fuchsia-600 text-white text-xl">{add}</button>
+            <button onClick={onClick} className="text-indigo-600 hover:text-fuchsia-600 text-2xl"><i class="fa-solid fa-trash"></i></button>
         </div>
     )
 }
